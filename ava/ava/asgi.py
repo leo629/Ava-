072c9a84970156a78f4203eac15be06847bca680
leo629 <williamfocus12@gmail.com>
2025-07-06 23:05:18 +0200

    Fix settings spc

--- ava/ava/asgi.py
+++ ava/ava/asgi.py
@@ -5,7 +5,7 @@ from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.auth import AuthMiddlewareStack
 import chat.routing
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'myproject.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ava.settings')
 
 application = ProtocolTypeRouter({
     "http": get_asgi_application(),
