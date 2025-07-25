d7928ef1b6034bf9574be233b0da462e10cb3ac3
leo629 <williamfocus12@gmail.com>
2025-07-25 03:38:07 +0200

    Touch runtime.txt to trigger rebuild

--- asgi.py
+++ asgi.py
@@ -5,7 +5,7 @@ from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.auth import AuthMiddlewareStack
 import chat.routing
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ava.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'settings')
 
 application = ProtocolTypeRouter({
     "http": get_asgi_application(),
