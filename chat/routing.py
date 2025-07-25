533b70043a6922d3ec18bda0b3ca0dc2239fce95
leo629 <williamfocus12@gmail.com>
2025-07-26 01:21:04 +0200

    using poetry

--- chat/routing.py
+++ chat/routing.py
@@ -1,6 +1,6 @@
 from django.urls import path
-from .consumers import ChatConsumer
+from . import consumers
 
 websocket_urlpatterns = [
-    path("ws/chat/<str:username>/", ChatConsumer.as_asgi()),
+    path("ws/chat/<str:username>/", consumers.ChatConsumer.as_asgi()),
 ]
