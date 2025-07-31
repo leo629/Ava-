8ecb5d7ba999816c604be3baad1986b2af5d22a2
leo629 <67419683+leo629@users.noreply.github.com>
2025-07-31 04:28:16 +0200

    Update settings.py

--- core/settings.py
+++ core/settings.py
@@ -86,8 +86,7 @@ CHANNEL_LAYERS = {
     'default': {
         'BACKEND': 'channels_redis.core.RedisChannelLayer',
         'CONFIG': {
-            "hosts": [('ava-nvqb.onrender.com')],
-            "password": url.password,
+            "hosts": [('ava-nvqb.onrender.com')]
             "ssl": True,
         },
     },
