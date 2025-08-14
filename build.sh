d340a42ec0c79a7bca5fb9e670d30dec1154cbff
leo629 <67419683+leo629@users.noreply.github.com>
2025-08-14 02:43:10 +0200

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,4 +1,10 @@
-#!/bin/bash
-poetry install
-python manage.py collectstatic --noinput
+h
+
+# Install dependencies
+pip install -r requirements.txt
+
+# Run migrations
 python manage.py migrate
+
+# Optional: collect static files if needed
+python manage.py collectstatic --noinput
